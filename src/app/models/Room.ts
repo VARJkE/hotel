59b6f9110a2021583ed90f50f8e2f2a17f4dfa1f
Lado Chopinovi <lado.chopinovi@gmail.com>
2023-11-01 20:56:30 +0400

    changed rooms images

--- src/app/models/Room.ts
+++ src/app/models/Room.ts
@@ -4,15 +4,13 @@ export class Room {
     id: number;
     roomNumber: number;
     roomType: number;
-    price: number;
     services: RoomType;
     isAvailable: boolean;
 
-    constructor(id: number, roomNumber: number, roomType: number, price: number, services: RoomType, isAvailable: boolean) {
+    constructor(id: number, roomNumber: number, roomType: number,  services: RoomType, isAvailable: boolean) {
         this.id = id,
         this.roomNumber = roomNumber,
         this.roomType = roomType,
-        this.price = price,
         this.services = services,
         this.isAvailable = isAvailable
     }
